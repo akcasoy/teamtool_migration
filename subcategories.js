1794a05203923c676670d490f293629963f53726
ozgurakcasoy <akcasoy@gmail.com>
2016-04-25 21:07:27 +0200

    Script is inserting new categories

--- subcategories.js
+++ subcategories.js
@@ -10,12 +10,26 @@ MongoClient.connect('mongodb://localhost:27017/agilemoves-dev', function(err, db
     cursor.each(function(err, trainingcard) {
         if (err) console.log("trainingcard cannot be read");
 
-        db.collection('categories').findOneAndUpdate({name:trainingcard.category}, {}, {upsert: true}, function(err, category) {
-            if(category == null) {
+        if(trainingcard == null) {
+            return 0;
+        }
 
-            }
-        });
+        if(trainingcard.category != null && trainingcard.category != undefined) {
 
+            db.collection('categories').findAndModify({name:trainingcard.category}, [['name', 1]], {name:trainingcard.category}, {new:true, upsert: true}, function(err, doc) {
+                if(err) console.log(err);
+                console.log("docccc = " + doc);
+            });
+
+
+            //db.collection('trainingcards').update({_id: trainingcard._id}, {
+            //    $unset : { 'category' : '' }}, function (err, updated) {
+            //
+            //    if (err) throw err;
+            //    console.dir(updated + " trainingcard Updated!");
+            //});
+
+        }
 
     });
 });
